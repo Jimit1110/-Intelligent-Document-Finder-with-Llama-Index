97ebed03cdb3602cd52883a8c55de5a68875d15e
Jimit <jimitnayakpara19@gmail.com>
2024-03-11 21:00:09 +0530

    updated for supporting folder limited search

--- setup_chromadb.py
+++ setup_chromadb.py
@@ -2,12 +2,21 @@
 
 import chromadb
 from llama_index.vector_stores.chroma import ChromaVectorStore
+import atexit
 
-def create_or_get_vectordb():
+def create_or_get_vectordb(caller=None):
     # It creates a persistent client with the specified path to store the database.
     chroma_client = chromadb.PersistentClient(path="chroma_db")
     # If the collection "quickstart" does not exist, it creates it
+
+    if caller == "call_from_pipeline":
+        try:
+            chroma_client.delete_collection(name="quickstart")
+        except:
+            pass 
+    
     chroma_collection = chroma_client.get_or_create_collection("quickstart")
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
 
+    atexit.register(lambda: chroma_client.reset())
     return vector_store
