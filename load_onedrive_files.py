d77aa4eddf4c2e048c966c9a9ad824d0196fe85f
Jimit <jimitnayakpara19@gmail.com>
2024-03-23 14:39:11 +0530

    change to load all files which are public

--- load_onedrive_files.py
+++ load_onedrive_files.py
@@ -8,7 +8,7 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 def load_data_from_onedrive(folder_id: str):
     loader = OneDriveReader(client_id=CLIENT_ID)
     #load documents from the specified OneDrive folder (recursive=False meaning don't interate through subfolders within folder)
-    documents = loader.load_data(folder_id=folder_id, recursive=False)
+    documents = loader.load_data(folder_id=folder_id, recursive=True)
     for doc in documents:
         doc.id_ = doc.metadata["file_name"]
     return documents
